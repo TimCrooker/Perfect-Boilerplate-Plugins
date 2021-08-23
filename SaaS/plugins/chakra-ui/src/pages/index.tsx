75a4446a873f02be989e9f32967307f5e897bb3a
Tim Crooker <timothycrooker@gmail.com>
2021-08-23 16:04:44 -0400

    format edit

--- SaaS/plugins/chakra-ui/src/pages/index.tsx
+++ SaaS/plugins/chakra-ui/src/pages/index.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Spacer, Flex } from "@chakra-ui/react";
+import React from 'react'
+import { Spacer, Flex } from '@chakra-ui/react'
 
-import { Header, Main, Cards, Footer } from "components";
+import { Header, Main, Cards, Footer } from 'components'
 
 const Home: React.FC = () => {
-    return (
-        <Flex direction="column" minH="100vh">
-            <Header />
-            <Main />
-            <Cards />
-            <Spacer />
-            <Footer />
-        </Flex>
-    );
-};
+	return (
+		<Flex direction="column" minH="100vh">
+			<Header />
+			<Main />
+			<Cards />
+			<Spacer />
+			<Footer />
+		</Flex>
+	)
+}
 
-export default Home;
+export default Home
