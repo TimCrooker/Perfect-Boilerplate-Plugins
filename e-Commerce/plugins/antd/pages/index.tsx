7dd2fff9d6863ce8bcbd6e670c564a88111a9a40
Tim Crooker <timothycrooker@gmail.com>
2021-08-23 18:42:15 -0400

    updated SaaS eslint plugin

--- e-Commerce/plugins/antd/pages/index.tsx
+++ e-Commerce/plugins/antd/pages/index.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react'
 
-import { Header, Main, Cards, Footer } from "@components";
+import { Header, Main, Cards, Footer } from '@components'
 
 const Home: React.FC = () => {
-    return (
-        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh"}}>
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
-        </div>
-    );
-};
+	return (
+		<div
+			style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+		>
+			<Header />
+			<Main />
+			<Cards />
+			<Footer />
+		</div>
+	)
+}
 
-export default Home;
+export default Home
