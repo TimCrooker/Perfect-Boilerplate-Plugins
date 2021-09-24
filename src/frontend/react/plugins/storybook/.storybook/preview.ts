b46bb60b0a733df655f65d12a372f22835d4d3e1
Tim Crooker <timothycrooker@gmail.com>
2021-09-24 19:55:48 -0400

    fixed styled-components storybook interop

--- src/frontend/react/plugins/storybook/.storybook/preview.ts
+++ src/frontend/react/plugins/storybook/.storybook/preview.ts
@@ -3,7 +3,7 @@
   import "../src/styles/global.css";
 <%_ } _%>
 <%_ if (ui.includes("bootstrap")) { _%>
-  import styles from "@/styles/app.scss";
+  import styles from "../src/styles/app.scss";
 <%_ } _%>
 <%_ if (css_features.includes("styled-components") || ui.includes("chakra-ui")) { _%>
 import { addDecorator } from "@storybook/react";
@@ -11,8 +11,8 @@ import { addDecorator } from "@storybook/react";
 <%_ if (css_features.includes("styled-components")) { _%>
   import { withThemesProvider } from "storybook-addon-styled-component-theme";
   
-  import { light } from "@/definitions/styled-components/light"
-  import { dark } from "@/definitions/styled-components/dark"
+  import { light } from "../src/definitions/styled-components/light"
+  import { dark } from "../src/definitions/styled-components/dark"
 <%_ } _%>
 
 <%_ if (ui.includes("chakra-ui")) { _%>
